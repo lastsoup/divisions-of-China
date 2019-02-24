92e88a34e0dd6a18dbd67e212d287807b3b7359e
lastsoup <qingtang166@qq.com>
2019-02-25 00:10:18 +0800

    add

--- src/components/index.vue
+++ src/components/index.vue
@@ -8,19 +8,21 @@
         <option value="3">乡级</option>
         <option value="4">村级</option>
       </select>
-      <button @click="all">开始抓取所有数据</button>
+      <button @click="all">开始抓取数据</button>
       <button @click="save">保存数据</button>
+      <span style="color:red;">注:最多获取到四级数据（乡级和村级可以数据可通过请求获取）</span>
      <div id="showdetail">
-         <p class="p">省级数据获取：共<span class="total">0</span>条</p>
-         <p class="c">地级数据获取：</p>
-         <p class="s">县级数据获取：</p>
-         <p class="t">乡级数据获取：</p>
-         <p class="v">村级数据获取：</p>
+         <p class="detail">数据获取情况：<span class="total">0</span>/<span class="count">0</span>
+         &nbsp; 获取<span class="remark" style="color:red;"></span>数据完毕！</p>
+     </div>
+      <div id="showerror">
      </div>
 </div>
 </template>
 <script>
-   var json=null;
+    var json=null;
+    var pcount=0;
+    var ptotal=0,ctotal=0,stotal=0,ttotal=0,vtotal=0;
     export default {
         data () {
             return {
@@ -33,57 +35,80 @@
         },
         methods:{
             save:function(){
+
+                json.push({'code':"71","name":"台湾省"});
+                json.push({'code':"81","name":"香港特别行政区"});
+                json.push({'code':"82","name":"澳门特别行政区"});
+                console.log(json);
                 $.post("/api/savejson",JSON.stringify(json),function(data){
                     alert(data);
                 });
             },
             all:function(){
-                var level=$("#level").get(0).selectedIndex;;
+                var level=$("#level").get(0).selectedIndex;
+                pcount=0;
                 switch(level){
                     case 0:
                     {
-                        this.Province(function(data){
-                            $("#showdetail .p .total").text(data.length);
-                            json=data;
-                        });
+                        this.Province(function(data){});
                         break;
                     }
                     case 1:
                     {
-                        this.City()
+                        this.City(function(data,n,item){})
+                        break;
+                    }
+                    case 2:
+                    {
+                        this.Area(function(data,n,item){
+                          
+                        })
                         break;
                     }
 
+
                 }
             },
-            ajaxHandler:function(url, param){
-                   return $.post(url, param).then(function (data) {
-                       return data;
-                    }, function (err) {
-                        return $.Deferred().reject(err);
-                    });
-            },
-            scrape:function (url,selecter,callback) {
-                this.ajaxHandler(this.api, { url: url, selecter:selecter}).done(function (data) {
-                    callback(data);
-                })
+            scrape:function (url,selecter,callback,error) {
+                   $.ajax({
+                        type: "POST",
+                        dataType: "json",
+                        url: this.api,
+                        data: { url: url, selecter:selecter}
+                    }).done(callback).fail(error);;
             },
             Province: function (callback) {
                 this.scrape(this.host,this.selecter[0],function(data){
+                    json=data;
+                    ptotal=data.length;
+                    $("#showdetail .detail .total").text(data.length);
                     callback(data);
                 });
             },
-            City:function(){
+            City:function(callback){
                 var dom=this;
                 var selecter=this.selecter;
-                dom.Province(function(data){
-                    $("#showdetail .p .total").text(data.length);
-                     $.each(data,function(){
-                        var url=dom.host+this.code+".html";
-                        dom.scrape(url,selecter[1],function(data){
-                            console.log(data);
+                dom.Province(function(pdata){
+                     $.each(pdata,function(n,item){
+                        var url=dom.host+item.code+".html";
+                        dom.scrape(url,selecter[1],function(cdata){
+                            pcount=pcount+1;
+                            item["children"]=cdata;
+                            json[n]=item;
+                            $("#showdetail .detail .count").text(pcount);
+                            $("#showdetail .detail .remark").text(item.name);
+                            callback(cdata,n,item);
+                        },function(err){
+                            console.log(n+";"+item.code+";"+item.name+";"+err.responseText);
                         });
                     });
+                });
+            },
+            Area:function(callback){
+                var dom=this;
+                var selecter=this.selecter;
+                dom.City(function(cdata,n,item){
+                   
                 });
             }
         }
